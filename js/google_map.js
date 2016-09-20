96c01af329a95565282a7c85e4556a2f76812755
SudhanvaMG <https://github.com/SudhanvaMG>
2016-09-20 23:01:28 +0530

    map changes

--- js/google_map.js
+++ js/google_map.js
@@ -4,14 +4,14 @@ var google;
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    // var myLatlng = new google.maps.LatLng(12.9094677, 77.5645786);
+    var myLatlng = new google.maps.LatLng(12.9094677,77.5645786);
     // 39.399872
     // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 17,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -35,7 +35,7 @@ function init() {
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
             var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            var latlng = new google.maps.LatLng(12.9094677,77.5645786);
             new google.maps.Marker({
                 position: latlng,
                 map: map,
